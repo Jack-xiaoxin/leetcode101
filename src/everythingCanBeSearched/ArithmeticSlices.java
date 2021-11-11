398b02835196068bc45e4bc1b4330cc09516c459
陈鑫 <915013255@qq.com>
2021-11-11 14:18:32 +0800

    增加了第413题，等差数列的个数

---
+++ src/everythingCanBeSearched/ArithmeticSlices.java
@@ -0,0 +1,17 @@
+package everythingCanBeSearched;
+
+public class ArithmeticSlices {
+
+    public int numberOfArithmeticSlices(int[] nums) {
+        if(nums.length <= 2 ) return 0;
+        int[] dp = new int[nums.length];
+        for(int i = 2; i < nums.length; i++) {
+            if(nums[i] - nums[i-1] == nums[i-1] - nums[i-2])
+                dp[i] = dp[i-1] + 1;
+        }
+        int result = 0;
+        for(int i = 0; i < dp.length; i++)
+            result += dp[i];
+        return result;
+    }
+}
