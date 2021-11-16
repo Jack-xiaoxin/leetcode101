2074f3b0bbbfe70a3bb9d3224afcf08082d00932
陈鑫 <915013255@qq.com>
2021-11-16 15:03:46 +0800

    01矩阵这道题增加了DP解法，先从左上到右下，再从右下到左上，说实话，没有BFS好理解

--- src/dynamicProgramming/ZeroOneMatric.java
+++ src/dynamicProgramming/ZeroOneMatric.java
@@ -9,7 +9,7 @@ public class ZeroOneMatric {
     public static void main(String[] args) {
         int[][] mat = new int[][]{{0,0,0},{0,1,0},{1,1,1}};
 //        int[][] mat = new int[][]{{0}, {1}};
-        int[][] result = new ZeroOneMatric().updateMatrix(mat);
+        int[][] result = new ZeroOneMatric().updateMatrix1(mat);
         for(int[] row : result) {
             System.out.println(Arrays.toString(row));
         }
@@ -48,5 +48,28 @@ public class ZeroOneMatric {
             level++;
         }
         return result;
+    }
+
+    public int[][] updateMatrix1(int[][] mat) {
+        int[][] dp = new int[mat.length][mat[0].length];
+        for(int[] row : dp) {
+            Arrays.fill(row, Integer.MAX_VALUE-10000);
+        }
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[i].length; j++) {
+                if(mat[i][j] == 0) dp[i][j] = 0;
+                else {
+                    if(i > 0) dp[i][j] = Math.min(dp[i-1][j]+1, dp[i][j]);
+                    if(j > 0) dp[i][j] = Math.min(dp[i][j-1]+1, dp[i][j]);
+                }
+            }
+        }
+        for(int i = 0; i < mat.length; i++) {
+            for(int j = 0; j < mat[i].length; j++) {
+                if(i < mat.length-1) dp[i][j] = Math.min(dp[i][j], dp[i+1][j]+1);
+                if(j < mat.length-1) dp[i][j] = Math.min(dp[i][j], dp[i][j+1]+1);
+            }
+        }
+        return dp;
     }
 }
