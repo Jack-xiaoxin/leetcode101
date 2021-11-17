45b453b53fa9b738f5b914faa5b362464457f490
陈鑫 <915013255@qq.com>
2021-11-17 10:24:58 +0800

    完全平方数增加了DFS的做法，果然不太好，超出内存限制了

--- src/dynamicProgramming/NumSquare.java
+++ src/dynamicProgramming/NumSquare.java
@@ -1,10 +1,11 @@
 package dynamicProgramming;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import java.util.Arrays;
 
 public class NumSquare {
-
-    int result = 0;
 
     //dp[i]不由临近的元素确定，而是min(dp[i-1], dp[i-4], dp[i-9]...)
     public int numSquares(int n) {
@@ -17,5 +18,28 @@ public class NumSquare {
             }
         }
         return dp[n];
+    }
+
+    //试试用DFS
+    public int numSquare(int n) {
+        List<Integer> list = new ArrayList<>();
+        for(int j = 1; j * j <= n; j++) {
+            DFS(n-j*j, list, 1);
+        }
+        int min = Integer.MAX_VALUE;
+        for(int count : list) {
+            if(min > count) min = count;
+        }
+        return min;
+    }
+
+    public void DFS(int n, List<Integer> list, int count) {
+        if(n == 0) {
+            list.add(count);
+            return ;
+        }
+        for(int j = 1; j * j <= n; j++) {
+            DFS(n-j*j, list, count+1);
+        }
     }
 }
